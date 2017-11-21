c98e7d3d283ca3d835ffc0c3e5966b2a74ab1e70
Alex J. Champandard <alexjc@aigamedev.com>
2017-11-21 20:55:51 +0100

    Switching production/consumption to integer resources, adding tests.

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import string
 import random
 import collections
 
@@ -5,12 +6,21 @@ import collections
 Request = collections.namedtuple('Request', ['neighbor', 'resource', 'value'])
 
 
-class Bot(object):
+class Config(object):
 
     def __init__(self):
-        self.inventory = None
+        self.production_per_tick = {i: 1 for i in random.sample(string.ascii_uppercase, 4)}
+        self.consumption_per_tick = {i: 1 for i in random.sample(string.ascii_uppercase, 4)}
 
-    def request_trade(self, neighbors, resources):
+
+class Bot(object):
+    """Base class for any AI written for the simulation.
+    """
+
+    def __init__(self):
+        self.config = None              #: Configuration for this agent.
+
+    def request_trade(self, neighbors, inventory):
         """Return a list of trade requests to make.
         """
         raise NotImplementedError
@@ -21,10 +31,10 @@ class Bot(object):
 
 class RandomBot(Bot):
 
-    def request_trade(self, neighbors, resources):
+    def request_trade(self, neighbors, inventory):
         """Return a list of requests to make.
         """
-        i, v = random.choice(resources)
+        i, _ = random.choice(inventory)
         n = random.choice(neighbors)
         return [Request(neighbor=n, resource=i, value=1.0)]
 
@@ -34,14 +44,17 @@ class RandomBot(Bot):
 
 class Agent(object):
 
-    def __init__(self, bot, health=1.0, happiness=0.0):
+    def __init__(self, bot, config, health=1.0, happiness=0.0):
         self.bot = bot
+        self.config = config
         self.health = health
         self.happiness = happiness
-        self.inventory = [0.0]
+
+        items = set(self.config.consumption_per_tick.keys()) | set(self.config.production_per_tick.keys())
+        self.inventory = {i: 0 for i in items}
         self.requests = []
         self.neighbors = []
-    
+
     def is_healthy(self):
         return self.health > 0.0
 
@@ -51,17 +64,17 @@ class Agent(object):
     def produce(self):
         """Agent produces goods at a fixed rate per turn.
         """
-        for i, _ in enumerate(self.inventory):
-            self.inventory[i] += 1.0
+        for item, value in self.config.production_per_tick.items():
+            self.inventory[item] += value
 
     def consume(self):
         """Agent consumes good at a fixed rate per turn.
         """
-        for i, c in enumerate(self.inventory):
-            if c >= 1.0:
-                self.inventory[i] -= 1.0
+        for item, value in self.config.consumption_per_tick.items():
+            if self.inventory[item] >= value:
+                self.inventory[item] -= value
             else:
-                self.health -= 1.0
+                self.health -= value
 
     def request_trade(self):
         """For all required resources, submit a request to a random agent.
@@ -69,7 +82,7 @@ class Agent(object):
         if len(self.neighbors) == 0:
             return
 
-        requests = self.bot.request_trade(list(self.neighbors.keys()), list(enumerate(self.inventory)))
+        requests = self.bot.request_trade(list(self.neighbors.keys()), list(self.inventory.items()))
         for r in requests:
             request = Request(neighbor=self, resource=r.resource, value=r.value)
             r.neighbor.requests.append(request)
@@ -81,7 +94,7 @@ class Agent(object):
             return
 
         for r in self.bot.accept_trade(self.requests):
-            if self.inventory[r.resource] < r.value:
+            if self.inventory.get(r.resource, 0) < r.value:
                 continue
 
             self.inventory[r.resource] -= r.value
@@ -94,7 +107,7 @@ class Agent(object):
 class Simulation(object):
 
     def __init__(self, agent_count=100, agent_health=1.0):
-        self.agents = [Agent(bot=RandomBot(), health=agent_health) for _ in range(agent_count)]
+        self.agents = [Agent(bot=RandomBot(), config=Config(), health=agent_health) for _ in range(agent_count)]
 
     def connect_all(self):
         for a in self.agents:
